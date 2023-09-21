fa08be12d52be2fbbf25c2719f8952b6cfbab6b8
Sebastian Bauhaus <S.Bauhaus@wbg-luenen.de>
2023-09-21 16:12:02 +0200

    Umbau auf neuen Wowicache

--- swserver/app.py
+++ swserver/app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify, json
 from dotenv import dotenv_values
-from wowipy.wowipy import WowiPy, Contractor, Person
+from wowicache.models import WowiCache, UseUnit, Person, Contractor, Communication, Address, Contract
+from sqlalchemy import or_, and_
 from datetime import datetime
 import sqlite3
 import os
@@ -25,24 +26,12 @@ log_levels = {'debug': 10, 'info': 20, 'warning': 30, 'error': 40, 'critical': 5
 app.logger.setLevel(log_levels.get(log_level, 20))
 sys.excepthook = handle_unhandled_exception
 if log_method == "file":
-    logging.basicConfig(filename=os.path.join(app.root_path, "log", "swserver"))
+    logging.basicConfig(filename=os.path.join(app.root_path, "log", "swserver.log"))
 elif log_method == "graylog":
     graylog_host = settings.get("graylog_host", "127.0.0.1")
     graylog_port = int(settings.get("graylog_port", 12201))
     handler = graypy.GELFUDPHandler(graylog_host, graylog_port)
     app.logger.addHandler(handler)
-
-# Wowiport-Daten einlesen
-wowi_host = settings.get("wowi_host")
-wowi_user = settings.get("wowi_user")
-wowi_pass = settings.get("wowi_pass")
-wowi_key = settings.get("wowi_key")
-
-wowi = WowiPy(wowi_host, wowi_user, wowi_pass, wowi_key, logger=app.logger)
-# catalogs = wowi.get_communication_catalogs()
-cache_contractors = settings.get("cache_contractors")
-cache_use_units = settings.get("cache_use_units")
-cache_use_persons = settings.get("cache_persons")
 
 search_base = settings.get("search_base", "person").lower()
 prefer_contract_address_str = settings.get("prefer_contract_address")
@@ -53,10 +42,7 @@ if prefer_contract_address_str is not None and len(prefer_contract_address_str) 
 
 prefer_use_unit_type = settings.get("prefer_use_unit_type")
 
-app.logger.info("swserver started. CACHE_READ")
-wowi.cache_from_disk(cache_type=wowi.CACHE_CONTRACTORS, file_name=cache_contractors)
-wowi.cache_from_disk(cache_type=wowi.CACHE_USE_UNITS, file_name=cache_use_units)
-wowi.cache_from_disk(cache_type=wowi.CACHE_PERSONS, file_name=cache_use_persons)
+app.logger.info("swserver started.")
 
 
 def get_token(header):
@@ -70,28 +56,8 @@ def get_token(header):
         return None
 
 
-@app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
-
-
 @app.route('/caller_info', methods=['GET'])
 def caller_info():
-    # Rückgabe-JSON in etwa:
-    # {
-    #   "idNum": "012345",
-    #   "id": 123,
-    #   "firstName": "Vorname",
-    #   "lastName": "Nachname",
-    #   "address": {
-    #       "street": "Bla",
-    #   },
-    #   "contracts": {
-    #       (vertrag)
-    #   },
-    #   "last_call": "2022-12-13",
-    #   "last_call_recipient": "Sachbearbeiter",
-    #   "last_call_reason": "Grund des Anrufs" (falls Wowiport Ticket)
     phone = request.args.get("phone")
     if phone is None or len(phone) == 0:
         app.logger.warning(f"phone arg missing. Client {request.remote_addr}, args {' '.join(request.args)}")
@@ -111,12 +77,12 @@ def caller_info():
     cur = con.cursor()
 
     query = "SELECT enabled FROM clients WHERE hostname=?"
-    cur.execute(query, (clientname, ))
+    cur.execute(query, (clientname,))
 
     clientrow = cur.fetchone()
     if not clientrow:
         query = "INSERT INTO clients(hostname) VALUES(?)"
-        cur.execute(query, (clientname, ))
+        cur.execute(query, (clientname,))
         con.commit()
         clientrow = [1]
 
@@ -126,50 +92,36 @@ def caller_info():
         app.logger.info(f"Client {clientname} ({request.remote_addr}) is disabled.")
         return 'client_disabled', 200
 
-    # DIe Normalisierung der Telefonnummern wird bereits in der entsprechenden Methode
-    # von WowiPy übernommen
+    db = WowiCache(settings.get("db_connection_string"))
 
     phone = phone.replace(' 49', '0')
     app.logger.debug(f"Client {clientname} ({request.remote_addr}) submitted phone number {phone}")
     app.logger.debug(f"Search Base: {search_base}")
+
+    comm_found: Communication
+    comm_found = db.session.query(Communication).filter(and_(or_(Communication.communication_type_id == 1,
+                                                                 Communication.communication_type_id == 3)),
+                                                        Communication.content.like(f'%{phone}%')).first()
+    if not comm_found:
+        app.logger.debug("No entry found.")
+        return 'caller_not_found', 404
+
     first_person: Person
-    if search_base == "contractor":
-        caller_found = wowi.search_contractor(search_phone=phone, allow_duplicates=True)
-        if len(caller_found) > 0:
-            first_contractor: Contractor
-            first_contractor = caller_found[0]
-            first_person = first_contractor.person
-        else:
-            app.logger.debug("No entry found.")
-            return 'caller_not_found', 404
-    else:
-        caller_found = wowi.search_person(search_phone=phone)
-        if len(caller_found) > 0:
-            first_person = caller_found[0]
-        else:
-            app.logger.debug("No entry found.")
-            return 'caller_not_found', 404
+    first_person = db.session.query(Person).get(comm_found.person_id)
 
     if first_person.is_natural_person:
-        if first_person.natural_person is not None:
-            first_name = first_person.natural_person.first_name
-            last_name = first_person.natural_person.last_name
-        else:
-            first_name = None
-            last_name = None
+        first_name = first_person.first_name
+        last_name = first_person.last_name
     else:
-        if first_person.legal_person is not None:
-            first_name = first_person.legal_person.long_name2
-            last_name = first_person.legal_person.long_name1
-        else:
-            first_name = None
-            last_name = None
+        first_name = first_person.long_name2
+        last_name = first_person.long_name1
 
     if first_person.addresses is not None:
+        first_address: Address
         first_address = first_person.addresses[0]
         addr = {
             'street': first_address.street_complete,
-            'postcode': first_address.zip_,
+            'postcode': first_address.postcode,
             'city': first_address.town
         }
     else:
@@ -179,43 +131,46 @@ def caller_info():
     address_overwritten_by_contract = False
     contract_list = []
     cont: Contractor
-    contractors_found = wowi.get_contractors(person_id=first_person.id_, use_cache=True)
+    contractors_found = db.session.query(Contractor).filter(Contractor.person_id == first_person.internal_id).all()
     for cont in contractors_found:
-        if cont.end_of_contract is not None:
-            if type(cont.end_of_contract) == str:
-                eoc = datetime.strptime(str(cont.end_of_contract), "%Y-%m-%d")
+        tcontract: Contract
+        tcontract = cont.contract
+        end_of_contract = tcontract.contract_end
+        if end_of_contract is not None:
+            if type(end_of_contract) == str:
+                eoc = datetime.strptime(str(end_of_contract), "%Y-%m-%d")
             else:
-                eoc = cont.end_of_contract
+                eoc = end_of_contract
             if datetime.today() > eoc:
                 continue
         if prefer_contract_address:
             overwrite_address = True
-            use_unit_idnum = cont.use_unit.use_unit_number
-            use_unit_obj = wowi.get_use_units(use_unit_idnum=use_unit_idnum)
-            if use_unit_obj is not None and len(use_unit_obj) > 0:
+            use_unit_obj: UseUnit
+            use_unit_obj = cont.use_unit
+            if use_unit_obj is not None:
                 if address_overwritten_by_contract:
-                    use_unit_type = use_unit_obj[0].current_use_unit_type.use_unit_usage_type.name
+                    use_unit_type = use_unit_obj.use_unit_usage_type
                     if use_unit_type != prefer_use_unit_type:
                         overwrite_address = False
 
                 if overwrite_address:
                     address_overwritten_by_contract = True
                     addr = {
-                        'street': use_unit_obj[0].estate_address.street_complete,
-                        'postcode': use_unit_obj[0].estate_address.zip_,
-                        'city': use_unit_obj[0].estate_address.town,
-                        'position': use_unit_obj[0].description_of_position
+                        'street': use_unit_obj.street_complete,
+                        'postcode': use_unit_obj.postcode,
+                        'city': use_unit_obj.town,
+                        'position': use_unit_obj.description_of_position
                     }
 
         t_contract = {
-            "Id": cont.license_agreement_id,
-            "IdNum": cont.license_agreement
+            "Id": cont.contract_id,
+            "IdNum": cont.contract.id_num
         }
         contract_list.append(t_contract)
 
     ret_dict = {
         "IdNum": first_person.id_num,
-        "Id": first_person.id_,
+        "Id": first_person.internal_id,
         "FirstName": first_name,
         "LastName": last_name,
         "Address": addr,
@@ -246,7 +201,7 @@ def clients(client_id: str = None):
     if request.method.upper() == 'GET':
         if clid > 0:
             query = "SELECT * FROM clients WHERE id=? ORDER BY id"
-            cur.execute(query, (clid, ))
+            cur.execute(query, (clid,))
         else:
             query = "SELECT * FROM clients ORDER BY id"
             cur.execute(query)
@@ -278,7 +233,7 @@ def clients(client_id: str = None):
             pass
             # Client update
             query = "SELECT 1 FROM clients WHERE id=?"
-            cur.execute(query, (clid, ))
+            cur.execute(query, (clid,))
             if not cur.fetchone():
                 return 'client_id_not_found', 404
 
